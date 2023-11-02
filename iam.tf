66d11ec2a0de8fd2f0f1aed2716411a2ffd8402d
veeranki2014 <veeranki2014@gmail.com>
2023-11-02 11:21:57 -0500

    terraform app module setup

--- iam.tf
+++ iam.tf
@@ -17,11 +17,10 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameter",
           "kms:Decrypt"
         ],
-        "Resource": [
+        "Resource": concat([
           "arn:aws:ssm:us-east-1:155405255921:parameter/roboshop.${var.env}.${var.component}.*",
-          "arn:aws:ssm:us-east-1:155405255921:parameter/roboshop.${var.env}.docdb.*",
           var.kms_key_arn
-          ]
+          ], var.extra_param_access)
       }
     ]
   })
--- variables.tf
+++ variables.tf
@@ -18,3 +18,4 @@ variable "lb_dns_name" {}
 variable "listener_arn" {}
 variable "lb_rule_priority" {}
 variable "kms_key_arn" {}
+variable "extra_param_access" {}
